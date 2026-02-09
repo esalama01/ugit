ea0be9af81ee280868d6680a498e6603c557187c
El mehdi Salama <mehdisalama61@gmail.com>
2026-02-09 22:02:00 +0100

    ugit init finished & worked

--- init.go
+++ init.go
@@ -1,21 +1,24 @@
 package main
-import(
+
+import (
 	"fmt"
 	"strconv"
+
 	//"io/fs"
-	"os"
 	"log"
+	"os"
+
 	//"path/filepath"
 	"gopkg.in/ini.v1"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
-func mkdirrr(){
+func mkdirrr() {
 	//creating the .ugit directory
 	err := os.Mkdir(".ugit", 0755)
 	check(err)
@@ -37,7 +40,12 @@ func mkdirrr(){
 	check(err)
 }
 
-func create(){
+func create() {
+
+	targetDir := ".ugit/" //go inside the .ugit directory
+	if err := os.Chdir(targetDir); err != nil {
+		log.Fatalf("Error : %v\n", err)
+	}
 	//creating the HEAD file (empty)
 	file_name := "HEAD"
 	p, err := os.Create(file_name)
@@ -50,30 +58,35 @@ func create(){
 	config(p)
 	check(err)
 	defer p.Close()
-	
+
 	//creating the description file (empty)
 	file_name = "description"
 	p, err = os.Create(file_name)
 	description(p)
 	check(err)
 	defer p.Close()
+	
+	targetDir = ".." //go back to the parent directory
+	if err := os.Chdir(targetDir); err != nil {
+		log.Fatalf("Error : %v\n", err)
+	}
 }
 
-func config(file *os.File){
+func config(file *os.File) {
 	cfg, err := ini.Load(file)
-	if err != nil{
+	if err != nil {
 		log.Fatalf("Something's wrong with the config file.")
 	}
-/*
-	core_section := cfg.Section("core")
-	core_section.Key("repositoryformatversion").SetValue(0)
-	core_section.Key("filemode").SetValue(true)
-	core_section.Key("bare").SetValue(false)
-	core_section.Key("logallrefupdates").SetValue(true)
-*/
+	/*
+		core_section := cfg.Section("core")
+		core_section.Key("repositoryformatversion").SetValue(0)
+		core_section.Key("filemode").SetValue(true)
+		core_section.Key("bare").SetValue(false)
+		core_section.Key("logallrefupdates").SetValue(true)
+	*/
 	//creating the core section
 	newSection, err := cfg.NewSection("core")
-	if err != nil{
+	if err != nil {
 		log.Fatalf("Cannot create the new section.")
 	}
 	//adding mappings between keys and values.
@@ -81,7 +94,7 @@ func config(file *os.File){
 	if err != nil {
 		log.Fatalf("Fail to add new key to new section: %v", err)
 	}
-	
+
 	_, err = newSection.NewKey("filemode", strconv.FormatBool(true))
 	if err != nil {
 		log.Fatalf("Fail to add new key to new section: %v", err)
@@ -92,24 +105,24 @@ func config(file *os.File){
 		log.Fatalf("Fail to add new key to new section: %v", err)
 	}
 
-	_, err = newSection.NewKey("logallrefupdates",strconv.FormatBool(true))
+	_, err = newSection.NewKey("logallrefupdates", strconv.FormatBool(true))
 	if err != nil {
 		log.Fatalf("Fail to add new key to new section: %v", err)
 	}
 
-	if err := cfg.SaveTo("config.ini"); err != nil{
+	if err := cfg.SaveTo("config.ini"); err != nil {
 		log.Fatalf("Failed to save the configuration file.")
 	}
 }
 
-func description(file *os.File){
+func description(file *os.File) {
 	data := []byte("Unnamed repository; edit this file 'description' to name the repository.")
 	err := os.WriteFile(file.Name(), data, 0644)
-	if err != nil{
+	if err != nil {
 		log.Fatalf("Couldn't write data to the description file.")
 	}
 }
-func Init(){
+func Init() {
 	mkdirrr()
 	create()
 	fmt.Println("The .ugit folder created succesfully.")
