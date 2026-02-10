b4b34ed9ff7dec43c79abd30137da41d60faa5c1
El mehdi Salama <mehdisalama61@gmail.com>
2026-02-10 20:42:24 +0100

    ugit objects implemented

--- objects.go
+++ objects.go
@@ -1,7 +1,26 @@
+import "encoding/json"
+
 type Blob struct {
-	hash_value string
+	h_id string
+	value string
 }
 
 type Tree struct {
-	hash_value string
+	h_id string
+	blob map[string]*string //map between blob names and pointers to their ids inside this tree 
+	subtree map[string]*string //maps between subtrees names and pointer of their id's inside this tree
+}
+
+type User struct {
+	user_name string `json:"username"`
+	user_email string `json:"email address"`
+	EventDate time.Time `json:"event_date"`
+}
+
+type Commit struct {
+	h_id string
+	Author *User
+	parent_tree *string //points to parent tree id
+	parents  []*string //slice of pointer to parents commit id's
+	message string
 }
