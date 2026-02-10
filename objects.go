587c971be28fda8bab1d21dc0869f2888ec5a710
El mehdi Salama <mehdisalama61@gmail.com>
2026-02-10 20:44:35 +0100

    objects sealed

--- objects.go
+++ objects.go
@@ -1,12 +1,12 @@
 import "encoding/json"
 
 type Blob struct {
-	h_id string
+	Blob_ID string //sha1 hash key of this blob
 	value string
 }
 
 type Tree struct {
-	h_id string
+	Tree_ID string //sha1 hash key of this tree
 	blob map[string]*string //map between blob names and pointers to their ids inside this tree 
 	subtree map[string]*string //maps between subtrees names and pointer of their id's inside this tree
 }
@@ -18,7 +18,7 @@ type User struct {
 }
 
 type Commit struct {
-	h_id string
+	Commit_ID string //sha1 hash key of this commit
 	Author *User
 	parent_tree *string //points to parent tree id
 	parents  []*string //slice of pointer to parents commit id's
