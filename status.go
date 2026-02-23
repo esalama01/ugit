5c865db301ef9c27a3e9c57278d4cc99365c528b
El mehdi Salama <mehdisalama61@gmail.com>
2026-02-23 23:17:55 +0000

    added teh difference func to calculate the diff between two arrays of strings

--- status.go
+++ status.go
@@ -1,10 +1,24 @@
 package main
 import (
-	"os"
 	"path/filepath"
 	"fmt"
 	"io/fs"
+	"os"
 )
+func difference(a, b []string) []string {// a functionn to calculate the difference between two slices of strings.
+    mb := make(map[string]struct{}, len(b))
+    for _, x := range b {
+        mb[x] = struct{}{}
+    }
+    var diff []string
+    for _, x := range a {
+        if _, found := mb[x]; !found {
+            diff = append(diff, x)
+        }
+    }
+    return diff
+}
+
 /*
 	i will be implementing the git status command. But how?
 	I ll have to scan the working directory and the stgaingarea and make a comparison between the files.
@@ -40,6 +54,33 @@ func traversal()([]string){ // a function that stores the path of each file in m
 	return paths
 }
 
-func compare(area *StagingArea) {
+func compare(area *StagingArea, paths []string) (int, string){//a function to compare whatever s in the index file with my current working directory.
 	
+	var exists []string //i ll need it to ppoint to removed files
+	//i ll traverse the paths slice  and check if each path exists in my index
+	for _, path := range paths {
+		f, err := os.Open(path) //open the file to pass it as a parameter in the Get_Hash function.
+		check(err)
+		defer f.Close()
+		value, ok := area.entries[path]
+		if ok { //if it exists
+			val1, _ := Get_Hash(f)
+			leftovers = append(leftovers, path)
+			if val1 == value.Id{ //if they re the same
+				return 2, path
+			}else{
+				return 3,path
+			}
+		}else{//if it doesn t eexist
+			return 1, path
+		}
+	}
+	//now i ll look for files that should be removed
+	keys := make([]int, len(area.entries)) //a slice containig the keys from my map.
+	i := 0
+	for k := range area.entries {
+    keys[i] = k
+    i++
+	}
+	//i ll substract 
 }
