0cf6c298dd1ab9165a081d57bcbb9e99603fb423
El mehdi Salama <mehdisalama61@gmail.com>
2026-03-02 01:25:29 +0000

    the trie ds is perfecct.

--- tree.go
+++ tree.go
@@ -2,6 +2,7 @@ package main
 import(
 	"os"
 	"strings"
+	"path/filepath"
 )
 //i will construct the tree first ny assigning names to it and adding subtrees and blobs relatiopns by using the trie ds, and after that i will make the tree_ID by using a traversal algorithm.(Merkel)
 
@@ -41,13 +42,22 @@ func Tree_construction(entry []string, t *Trie){ //a function to implement a wor
 	}
 }
 
+func directory_name()(string){
+	dir, err := os.Getwd()
+	check(err)
+
+	// Extract just the name of the directory from the full path
+	dirName := filepath.Base(dir)
+	return dirName
+
+}
 
 //i'll be implementing the git write-tree command.
 func Ugit_write_tree(){//the function takes as input the index file and reads it.
 
 	//--------------Initializing an empty tree-------------------------------------------
 	my_tree := new(Tree)
-	my_trie := new(Trie)
+	my_trie := NewTrie(directory_name())
 	//-----------------------------------------------------------------------------------
 
 	//--------------Loading the staging area along with the paths slice------------------
@@ -61,7 +71,7 @@ func Ugit_write_tree(){//the function takes as input the index file and reads it
 	//---------------slicing each path to construct a tree-------------------------------
 	for _, path := range paths{
 		entry := strings.Split(path, "/")
-
+		Tree_construction(entry , my_trie)
 	}
 	//-----------------------------------------------------------------------------------
 }
