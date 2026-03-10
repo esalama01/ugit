775391e2ed4eb6c22ed28d0bb9dff3f94c7535aa
El mehdi Salama <mehdisalama61@gmail.com>
2026-03-10 12:48:57 +0000

    comprasion function implemented

--- diff.go
+++ diff.go
@@ -9,6 +9,44 @@ import(
 	"io"
 )
 
+type Sha1_path struct{
+	Checksum string
+	Path string
+}
+
+
+func Compare_with_sha1() ([]*Sha1_path){//a function to compare whatever s in the index file with my current working directory. But adds the sha1 of the file(the one in the index) to the result
+	area := StagingArea{
+        entries: make(map[string]*Index),
+    }
+	Loadfromtheindex(&area)
+	paths := Traversal()
+	var m []*Sha1_path
+	var exists []string //i ll need it to point to removed files
+	//i ll traverse the paths slice  and check if each path exists in my index
+	for _, path := range paths { 
+		f, err := os.Open(path) //open the file to pass it as a parameter in the Get_Hash function.
+		check(err)
+		value, ok := area.entries[path]
+		if ok { //if it exists
+			val1, _ := Get_Hash_Blob(f)
+			f.Close()
+			exists = append(exists, path)
+			if val1 != value.Id{ //if they re the same
+				instance := Sha1_path{
+					Checksum : value.Id,
+					Path : path,
+				}
+				m = append(m, &instance)
+			}else{
+			}
+		}else{//if it doesn t exist
+		}
+		
+	}
+	return m
+}
+
 func Ugit_cat_file(path string) string{
 	content, err := os.ReadFile(path)
 	check(err)
@@ -20,6 +58,6 @@ func Ugit_cat_file(path string) string{
 	var sb strings.Builder
 	_, err = io.Copy(&sb, r)
 	check(err)
-	result := sb.String()
-	return result
+	text := sb.String()
+	//i need to remove the header from the text.
 }
